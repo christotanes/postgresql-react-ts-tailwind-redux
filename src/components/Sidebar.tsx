bba16bf0643bf2aa3c0ce29661e45e92d5f76527
christopher.totanes <christopher.totanes@gmail.com>
2024-03-01 03:14:50 +0800

    Fixed active classes of Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,23 +1,38 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [activeIndex, setActiveIndex] = useState("");
   const links = [
     { label: "Users", path: "/users" },
     { label: "Login", path: "/login" },
     { label: "Logout", path: "/" },
   ];
 
+  const activeClasses =
+    "text-blue-500 pl-3 font-bold border-l-cyan-900 border-l-2";
+
+  const classes = "text-blue-500 pl-2 border-l hover:border-l-cyan-500";
+
+  const handleClick = (label: string) => {
+    setActiveIndex(label);
+  };
+
   const renderedLinks = links.map((link) => {
     return (
       <Link
         to={link.path}
         key={link.label}
-        className="text-blue-500 border-l border-l-cyan-900 pl-2"
+        className={
+          activeIndex === link.path ? `${activeClasses}` : `${classes}`
+        }
+        onClick={() => handleClick(link.path)}
       >
         {link.label}
       </Link>
     );
   });
+
   return (
     <section className="flex flex-col h-screen mx-10 mt-10">
       {renderedLinks}
