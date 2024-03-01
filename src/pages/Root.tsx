d1d0da2bf961f2db62dcfd9f2ac5650367280f46
christopher.totanes <christopher.totanes@gmail.com>
2024-03-01 08:54:25 +0800

    Created Header and added to root

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 
 export default function Root() {
   return (
-    <div className="flex w-full h-screen">
-      <Sidebar />
-      <Outlet />
+    <div className="container mx-auto">
+      <Header />
+      <div className="flex">
+        <Sidebar />
+        <Outlet />
+      </div>
     </div>
   );
 }
