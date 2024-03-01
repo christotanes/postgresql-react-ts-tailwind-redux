4e712a7fca11097154538f7a2670b458a12f1c82
christopher.totanes <christopher.totanes@gmail.com>
2024-03-01 09:22:24 +0800

    Add input fields for LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,10 +4,21 @@ export default function LoginPage() {
   // const handleClick = () => {};
   return (
     <div className="w-full flex align-middle justify-items-center">
-      <div className="m-auto border border-slate-200 p-10 rounded shadow-lg">
-        <form>
+      <div className="mx-auto border-1 border-slate-200 p-5 rounded shadow-lg">
+        <form className="w-full flex flex-col">
           <h3>Login</h3>
           <label>Email:</label>
+          <input
+            className="border border-slate-700 mb-2 rounded pl-2"
+            type="email"
+            placeholder="Enter your Email"
+          />
+          <label>Password:</label>
+          <input
+            className="border border-slate-700 mb-2 rounded pl-2"
+            type="password"
+            placeholder="Enter your Password"
+          />
           <Button rounded primary>
             Login
           </Button>
