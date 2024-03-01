59397ab82b9dce18e2a61f1ca3104a13f22b261b
christopher.totanes <christopher.totanes@gmail.com>
2024-03-01 09:02:22 +0800

    Styled Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,14 @@
 export default function Header() {
-  return <div>This is header</div>;
+  return (
+    <div className="flex align-middle justify-between py-2">
+      <div className="ml-10">Brand</div>
+      <div className="mr-20">
+        <input
+          className="border border-slate-700 rounded pl-2 pr-10"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+    </div>
+  );
 }
